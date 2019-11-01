7bd61b70a33df2061484c9e8803ed49f105b401c
Administrator <Administrator@SC-201903081531>
2019-11-01 11:15:55 +0800

    这是Spring-Cloud分布式中的的commons项目

---
+++ sp08-hystrix-dashboard/src/test/java/com/tedu/sp08/Sp08HystrixDashboardApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tedu.sp08;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class Sp08HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
